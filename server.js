142cfa35b7a48376591aa03bbd50ad0495082920
)- <a-@-s-MacBook-Pro.local>
2022-05-22 19:41:04 -0400

    tip calculator route

--- server.js
+++ server.js
@@ -3,9 +3,18 @@ const app = express();
 
 
 const port = 3000;
+
 //Greeting Route
 app.get('/greeting/kevin', (req, res) => {
     res.send('Wow! Hello there, Jimmy boy')
+})
+
+//Tip Route
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    console.log(req.params)
+    let total = req.params.total;
+    let tip = req.params.tipPercentage;
+    res.send(`${(tip / total ) * 100}`)
 })
 
 app.listen(port, (req, res) => {
