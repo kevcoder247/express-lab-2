bb6d4554b3e09abcf712b2885a15cb6d5ce11964
)- <a-@-s-MacBook-Pro.local>
2022-05-30 14:38:58 -0400

    magic route added

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 
 const port = 3000;
 
+const magicBall = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
+
 //Greeting Route
 app.get('/greeting/kevin', (req, res) => {
     res.send('Wow! Hello there, Jimmy boy')
@@ -17,6 +19,16 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
     res.send(`${(tip / total ) * 100}`)
 })
 
+
+//Magic Route
+console.log(magicBall.length)
+//Magic Route
+app.get('/magic/:query', (req, res) => {
+    console.log(req.params.query )
+    res.send(`<h1>${req.params.query}? ${magicBall[Math.floor(Math.random() * magicBall.length)]}</h1>`)
+})
+
+
 app.listen(port, (req, res) => {
-    console.log('hello')
+    console.log('Listening on route', port)
 })
